cc8d475263b995ac647ebc9b6698e4d4fbe8a3fa
Marco Link <m.link@cern.ch>
2023-03-02 10:59:29 +0100

    add charge tagger calibration to fit model

--- python/WbWbXModel.py
+++ python/WbWbXModel.py
@@ -11,7 +11,8 @@ class DefaultWbWbXModel(PhysicsModelBase):
     Combine model for WbWbX fit with two parameters mtop and gammat
     """
 
-    def __init__(self, verbose=False):
+    def __init__(self, cTaggerCalibration=False, verbose=False):
+        self.cTaggerCalibration = cTaggerCalibration
         self.verbose = verbose
         self.gen_json = pandas.read_json('config/xsecs.json').T
         self.minmass = self.gen_json['mass'].min()
@@ -22,23 +23,30 @@ class DefaultWbWbXModel(PhysicsModelBase):
 
     def doParametersOfInterest(self):
         """Create POI out of signal strength and MH"""
+        pois = 'r,m_top,gamma_t'
         self.modelBuilder.doVar('r[1,0.0,10.0]')
         self.modelBuilder.doVar('m_top[172.5,{},{}]'.format(self.minmass, self.maxmass))
         self.modelBuilder.doVar('gamma_t[1.322,{},{}]'.format(self.minwidth, self.maxwidth))
 
-        self.modelBuilder.doSet('POI', 'r,m_top,gamma_t')
+        if self.cTaggerCalibration:
+            self.modelBuilder.doVar('cTagger_insitu_A[1,0.5,2.0]')
+            self.modelBuilder.doVar('cTagger_insitu_B[1,0.5,2.0]')
+            self.modelBuilder.doVar('cTagger_insitu_C[1,0.5,2.0]')
+            pois[0] += ',cTagger_insitu_A,cTagger_insitu_B,cTagger_insitu_C'
+
+        self.modelBuilder.doSet('POI', pois)
         self.setup()
 
 
     def getGridRange(self, mtop, gammat):
-        #"""
-        #Who doesn't want some support?
-        #Determines the support point in the mass/width grid.
+        """
+        Who doesn't want some support?
+        Determines the support point in the mass/width grid.
 
-        #:param mtop: value of the top mass TODO update docstring
-        #:param gammat: value of the top width
-        #:return: numpy array containing index and scale
-        #"""
+        :param mtop: value of the top mass TODO update docstring
+        :param gammat: value of the top width
+        :return: numpy array containing index and scale
+        """
         masspoints = numpy.unique(self.gen_json['mass'])
         widthpoints = numpy.unique(self.gen_json['width'])
 
@@ -74,7 +82,7 @@ class DefaultWbWbXModel(PhysicsModelBase):
             gammat = self.gen_json['width'][i]
             limits = self.getGridRange(mtop=mtop, gammat=gammat)
 
-            # switch off points unused points
+            # switch off unused points
             self.modelBuilder.factory_(('expr::Switch_WbWbX_{i}("(@0>{lmass})*'
                                         '(@0<{umass})*(@1>{lwidth})*(@1<{uwidth})",'
                                         'm_top, gamma_t)').format(i=i, lmass=limits[0], umass=limits[1],
@@ -95,12 +103,32 @@ class DefaultWbWbXModel(PhysicsModelBase):
         self.modelBuilder.factory_('expr::Normalization_WbWbX("@0/@1", r, {})'.format(last))
 
 
+        if self.cTaggerCalibration:
+            self.modelBuilder.factory_('expr::cTaggerCalibration_Bpos("@0", cTagger_insitu_A)')
+            self.modelBuilder.factory_('expr::cTaggerCalibration_Bneg("@0", cTagger_insitu_B)')
+            self.modelBuilder.factory_('expr::cTaggerCalibration_B0("@0", cTagger_insitu_C)')
+            self.modelBuilder.factory_('expr::cTaggerCalibration_B0bar("1/@0/@1/@2", cTagger_insitu_A, cTagger_insitu_B, cTagger_insitu_C)')
+
+
+
     def getYieldScale(self, bin, process):
+        calib = 1
+
+        if self.cTaggerCalibration:
+            print(bin)
+            taggerCategory = bin.split('_')[-2]
+            if taggerCategory not in ['Bpos', 'Bneg', 'B0', 'B0bar']:
+                raise(Exception('unnown charge tagger category: "{}" in bin "{}"'.format(taggerCategory, bin)))
+
+            calib = 'cTaggerCalibration_{}'.format(taggerCategory)
+
+
         if 'WbWbX_' in process:
-            self.modelBuilder.factory_('expr::FinalScale_{proc}("@0*@1", Scaling_{proc}, Normalization_WbWbX)'.format(proc=process))
+            self.modelBuilder.factory_('expr::FinalScale_{proc}("@0*@1*@2", Scaling_{proc},'.format(proc=process)
+                                       + ' Normalization_WbWbX, {calibration})'.format(calibration=calib))
             return 'FinalScale_' + process
         else:
-            return 1
+            return calib
 
 
 DefaultWbWbXModel = DefaultWbWbXModel()
